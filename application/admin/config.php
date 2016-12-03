b51f15fe7a5e289b88d317933eab90a5a6e13c5d
liyanpinghaha <liyanpinghaha.com>
2016-12-03 09:56:07 +0800

    'config'

---
+++ application/admin/config.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+	    // 视图输出字符串内容替换
+    'view_replace_str'       => [
+        '__ADMIN__SITE__' => 'http://www.goodjob.com',
+        // '__STATIC__' => 'http://static.tp5.com/static'
+        '__ADMIN__STATIC__' => 'http://www.goodjob.com/static/admin'
+    ]
+];
